2eecd2492aaa7a30c381a870a909f21cb0c629b4
Linton Ye <linton@jimulabs.com>
2017-09-22 09:31:36 -0700

    Add expression examples

---
+++ react_house/src/expression_examples.js
@@ -0,0 +1,62 @@
+
+let age = 10;
+let age = age + 1;
+let name = 'Tom';
+let pets = ['🐶','🐱','🐨'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const packFruits = () => ['🍉', '🥝', '🍒'];
+
+
+
+
+
+
+
+
+
+
+
+for (let i = 0; i< 10; i++) {
+  console.log('blah');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function packSnack(type) {
+  let snack;
+  if (type === 'sweets')
+    snack = '🍭🍩🍪';
+  else
+    snack = '🥕🍌';
+  return snack;
+}
