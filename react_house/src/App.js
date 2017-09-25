260cc9fdc549ab8caeb9e0f19a775211e6ea9852
Linton Ye <linton@jimulabs.com>
2017-09-25 09:46:53 -0700

    style the house with CSS

--- react_house/src/App.js
+++ react_house/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
+import Images from './Images';
 
 const House = () => (
-  <div>
+  <div className="house">
     <Roof />
     <Wall />
     <Window />
@@ -10,10 +11,10 @@ const House = () => (
   </div>
 );
 
-const Roof = () => <div>Roof</div>;
-const Wall = () => <div>Wall</div>;
-const Window = () => <div>Window</div>;
-const Door = () => <div>Door</div>;
+const Roof = () => <img src={Images.roof} className="roof" />;
+const Wall = () => <img src={Images.wall} className="wall" />;
+const Window = () => <img src={Images.window} className="window" />;
+const Door = () => <img src={Images.door_closed} className="door" />;
 
 const App = () => <House />;
 
