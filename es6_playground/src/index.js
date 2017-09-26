7d2f34d4fa17f38a098aff8217315968338f56b4
Linton Ye <linton@jimulabs.com>
2017-09-25 17:44:58 -0700

    Add dogs

--- es6_playground/src/index.js
+++ es6_playground/src/index.js
@@ -1,3 +1,12 @@
-// Insert your code in this file
+let dog1 = {
+  name: 'Max',
+  breed: 'Bulldog'
+};
 
-console.log('Hello world!👍');
+let dog2 = {
+  name: 'Bella',
+  breed: 'Poodle'
+};
+
+console.log('dog1: ', dog1.name, dog1.breed);
+console.log('dog2: ', dog2.name, dog2.breed);
