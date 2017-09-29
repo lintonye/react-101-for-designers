b3e9b2fec2effa7ec0c40956032884b2388b0b5a
Linton Ye <linton@jimulabs.com>
2017-09-28 18:20:00 -0700

    Make door openable

--- react_house/src/App.js
+++ react_house/src/App.js
@@ -41,14 +41,41 @@ const House = () => (
     <Roof />
     <Wall />
     <Window />
-    <Door />
+    <Door shape="rectangle" />
   </HouseDiv>
 );
 
+// functional component
+// stateless
 const Roof = () => <RoofImg src={Images.roof} />;
 const Wall = () => <WallImg src={Images.wall} />;
 const Window = () => <WindowImg src={Images.window} />;
-const Door = () => <DoorImg src={Images.door_closed} />;
+//const Door = () => <DoorImg src={Images.door_closed} />;
+
+// class component
+// inherit properties, methods
+// stateful
+// state
+class Door extends React.Component {
+  constructor() {
+    super();
+    this.state = { isOpen: false };
+  }
+  
+  render() {
+    const handleClick = () => {
+      if (this.state.isOpen)
+        this.setState({ isOpen: false });
+      else
+        this.setState({ isOpen: true });
+    }
+
+    const img = this.state.isOpen
+      ? Images.door_open 
+      : Images.door_closed;
+    return <DoorImg src={img} onClick={ handleClick }/>;
+  }
+}
 
 const App = () => <House />;
 
