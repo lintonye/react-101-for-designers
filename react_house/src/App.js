3a3ea54247e4188cf4fd75d4545c3ccf522d1c0c
Linton Ye <linton@jimulabs.com>
2017-10-16 08:20:58 -0700

    Extract style into object

--- react_house/src/App.js
+++ react_house/src/App.js
@@ -11,10 +11,18 @@ const House = () => (
   </div>
 );
 
-const Roof = () => <img src={Images.roof} className="roof" />;
-const Wall = () => <img src={Images.wall} className="wall" />;
-const Window = () => <img src={Images.window} className="window" />;
-const Door = () => <img src={Images.door_closed} className="door" />;
+const styles = {
+  roof: {
+    left: 0, 
+    top: 0,
+    marginLeft: 50,
+  }
+}
+
+const Roof = () => <img style={styles.roof} src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Froof.png?1501113882154" />;
+const Wall = () => <img className="wall" src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fwall.png?1501113882297" />;
+const Window = () => <img className="window" src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fwindow.png?1501113882112" />;
+const Door = () => <img className="door" src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fdoor_close.png?1501113881433" />;
 
 const App = () => <House />;
 
