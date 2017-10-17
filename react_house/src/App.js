03159137015d7446449009b09d6fdd56fa361c77
Linton Ye <linton@jimulabs.com>
2017-10-17 12:37:40 -0700

    Style wall instead

--- react_house/src/App.js
+++ react_house/src/App.js
@@ -12,15 +12,14 @@ const House = () => (
 );
 
 const styles = {
-  roof: {
-    left: 0, 
-    top: 0,
-    marginLeft: 50,
+  wall: {
+    left: '5px', 
+    top: '48px',
   }
 }
 
-const Roof = () => <img style={styles.roof} src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Froof.png?1501113882154" />;
-const Wall = () => <img className="wall" src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fwall.png?1501113882297" />;
+const Roof = () => <img className="roof" src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Froof.png?1501113882154" />;
+const Wall = () => <img style={styles.wall} src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fwall.png?1501113882297" />;
 const Window = () => <img className="window" src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fwindow.png?1501113882112" />;
 const Door = () => <img className="door" src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fdoor_close.png?1501113881433" />;
 
