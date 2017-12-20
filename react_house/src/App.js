798ec65e6f9be93dd342012812f8032bf2d6cb0c
Linton Ye <linton@jimulabs.com>
2017-12-20 12:46:31 -0800

    Screencast updates

--- react_house/src/App.js
+++ react_house/src/App.js
@@ -10,10 +10,11 @@ const House = () => (
   </div>
 );
 
-const Roof = () => <div>Roof</div>;
-const Wall = () => <div>Wall</div>;
-const Window = () => <div>Window</div>;
-const Door = () => <div>Door</div>;
+const Roof = () => <img className="roof" src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Froof.png?1501113882154" />;
+const Wall = () => <img className="wall" src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fwall.png?1501113882297" />;
+const Window = () => <img className="window" src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fwindow.png?1501113882112" />;
+const Door = () => <img className="door" src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fdoor_close.png?1501113881433" />;
+
 
 const App = () => <House />;
 
