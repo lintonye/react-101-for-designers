204ae99372311d2f342563b073ead400267370d3
Linton Ye <linton@jimulabs.com>
2017-12-20 12:32:33 -0800

    Screencast updates

--- es6_playground/src/index.js
+++ es6_playground/src/index.js
@@ -1,5 +1,12 @@
 import snacks, { makeJuice as makeAwesomeJuice, APPLE } from './fruits';
+import sweets from './sweets.json';
+
+import image from './images/dom.png';
 
 console.log(snacks);
 
 console.log(makeAwesomeJuice(APPLE));
+
+console.log(sweets);
+
+console.log(image);
