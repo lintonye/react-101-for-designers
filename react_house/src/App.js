8df0032eb7370eaaaabc879e66f8f1684dd676d6
Linton Ye <linton@jimulabs.com>
2017-12-22 13:18:14 -0800

    Screencast updates

--- react_house/src/App.js
+++ react_house/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Images from './Images';
+import styled from 'styled-components';
 
 const House = () => (
   <div className="house">
@@ -18,8 +19,13 @@ const styles = {
   }
 }
 
+const WallImg = styled.img`
+  left: 5px;
+  top: 48px;
+`;
+
 const Roof = () => <img className="roof" src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Froof.png?1501113882154" />;
-const Wall = () => <img style={styles.wall} src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fwall.png?1501113882297" />;
+const Wall = () => <WallImg src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fwall.png?1501113882297" />;
 const Window = () => <img className="window" src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fwindow.png?1501113882112" />;
 const Door = () => <img className="door" src="https://cdn.glitch.com/1fb3273a-81cb-45bc-acbd-555546cb098f%2Fdoor_close.png?1501113881433" />;
 
