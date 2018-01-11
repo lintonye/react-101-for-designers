de896daf9e19ae160353d7ad79db39216c25e446
Linton Ye <linton@jimulabs.com>
2018-01-10 16:35:11 -0800

    Add exercise instructions

--- react_house/src/App.js
+++ react_house/src/App.js
@@ -2,6 +2,26 @@ import React from 'react';
 import './App.css';
 import Images from './Images';
 
+/**
+ 
+Exercises:
+
+1. Add roof, wall, window and door to the house (all plain text, e.g. `<div>window</div>`)
+2. Display two houses, one after another. The expected output is:
+
+roof
+wall
+window
+door
+----------
+roof
+wall
+window
+door
+
+Hint: For the horizontal separator line between the houses, use `<hr />`.
+
+*/
 const House = () => (
   <div>
     House!
