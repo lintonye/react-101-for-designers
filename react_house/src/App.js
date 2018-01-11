1f1576712db0614b93c0fd90d37c522c34d2ba0d
Linton Ye <linton@jimulabs.com>
2018-01-10 17:47:49 -0800

    Add exercise 25

--- react_house/src/App.js
+++ react_house/src/App.js
@@ -5,65 +5,57 @@ import Images from './Images';
 /*
 
 Exercises:
-  - Make the window open/close when the user clicks on it
+  1. Make the window clickable, i.e. when you click on it, the window
+     should open or close just like the door. Do NOT convert the Window
+     component to class component.
+  2. Wake up the cat when the window or the door (or both) is open. 
+    If both the window and the door are closed, make the cat sleep.
 
 */
 
-const House = () => (
-  <div className="house">
-    <Roof color="brown" />
-    <Wall />
-    <Door />
-    <Window />
-  </div>
-);
+class House extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { doorOpen: false };
+  }
+  handleDoorClick = () => {
+    if (this.state.doorOpen === true) {
+      this.setState({ doorOpen: false });
+    } else {
+      this.setState({ doorOpen: true });
+    }
+  };
+  render() {
+    const catStatus = this.state.doorOpen ? 'awake' : 'sleeping';
+    return (
+      <div className="house">
+        <Roof color="brown" />
+        <Wall />
+        <Door onClick={this.handleDoorClick} isOpen={this.state.doorOpen} />
+        <Window />
+        <Cat status={catStatus} />
+      </div>
+    );
+  }
+}
+
+const Cat = (props) =>
+  <img src={props.status === 'sleeping' ? Images.cat_sleeping : Images.cat_standing}
+    className="cat" />
 
 const Roof = (props) => (
   <img src={Images.roof} className="roof" style={{ background: props.color }} />
 );
-const Wall = () => <img src={Images.wall} className="wall" />;
+const Wall = () => <img src={Images.wall} className="wall" />
 
-class Window extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isOpen: false };
-  }
-  render() {
-    const handleClick = () => {
-      if (this.state.isOpen) {
-        this.setState({ isOpen: false });
-      } else {
-        this.setState({ isOpen: true });
-      }
-    }
-    let image = Images.window_open;
-    if (this.state.isOpen === false) {
-      image = Images.window_closed;
-    }
-    return <img src={image} className="window" onClick={handleClick} />;
-  }
-}
+const Window = (props) =>
+  <img src={props.isOpen ? Images.window_open : Images.window_closed}
+    className="window" />
 
-class Door extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isOpen: false };
-  }
-  render() {
-    const handleClick = () => {
-      if (this.state.isOpen) {
-        this.setState({ isOpen: false });
-      } else {
-        this.setState({ isOpen: true });
-      }
-    }
-    let image = Images.door_open;
-    if (this.state.isOpen === false) {
-      image = Images.door_closed;
-    }
-    return <img src={image} className='door' onClick={handleClick} />;
-  }
-}
+const Door = (props) =>
+  <img src={props.isOpen ? Images.door_open : Images.door_closed}
+    className="door" onClick={props.onClick} />
+
 
 const App = () => <House />;
 
